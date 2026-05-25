ef0f15137a2302e9ed2e77847be53c8f9983665e
Mugunthan-127 <mugunthanmuthuraman9@gmail.com>
2026-05-25 21:15:05 +0530

    Completed frontend setup and routing

---
+++ frontend/src/pages/AddWorkout.jsx
@@ -0,0 +1,9 @@
+function AddWorkout() {
+  return (
+    <div>
+      <h1>Add Workout Page</h1>
+    </div>
+  );
+}
+
+export default AddWorkout;
---
+++ frontend/src/pages/History.jsx
@@ -0,0 +1,9 @@
+function History() {
+  return (
+    <div>
+      <h1>Workout History Page</h1>
+    </div>
+  );
+}
+
+export default History;
